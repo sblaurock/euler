b44c34e50319e35125d89c1a9dd814a942d3d775
sblaurock <sblaurock@gmail.com>
2013-07-19 00:50:18 -0400

    Completed question 6.

---
+++ 6/6.rb
@@ -0,0 +1,29 @@
+#time	0m0.003s
+
+limit = 100
+
+def sumOfSquares limit
+	_i = 0
+	_sum = 0
+
+	while _i <= limit do
+		_sum += _i * _i
+		_i += 1
+	end
+
+	return _sum
+end
+
+def squareOfSums limit
+	_i = 0
+	_sum = 0
+
+	while _i <= limit do
+		_sum += _i
+		_i += 1
+	end
+
+	return _sum * _sum
+end
+
+puts (squareOfSums limit) - (sumOfSquares limit)

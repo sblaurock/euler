140a4033404782e8d8b2c69275b6732b8d898a4c
sblaurock <sblaurock@gmail.com>
2013-07-22 01:20:36 -0400

    Completed question 9.

---
+++ 9/9.rb
@@ -0,0 +1,22 @@
+#time	0m0.084s
+
+limit = 1000
+loopLimit = limit / 2
+
+# Loop through possible combinations to find the triplet that matches our criterion
+a = 1
+while a < loopLimit
+	b = a + 1
+	while b < loopLimit
+		c = limit - a - b
+
+		if c * c - (a * a + b * b) === 0
+			puts a * b * c
+			exit
+		end
+
+		b += 1
+	end
+
+	a += 1
+end

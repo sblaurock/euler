cfa7caae09f248d1aaa9dd84215936d74b665ecc
sblaurock <sblaurock@gmail.com>
2013-08-03 02:29:55 -0400

    Completed question 13.

---
+++ 13/13.rb
@@ -0,0 +1,11 @@
+#time	0m0.003s
+
+sum = 0
+digits = 10
+
+# Read from our input file, adding the significant digits on each line to 'sum'
+File.open("input", "r").each_line do |line|
+	sum += line.to_s[0, digits + 1].to_i
+end
+
+puts sum.to_s[0, digits]

87acbd1ac6c0707b1fb511cc205c245b5c5e3d33
Steve Blaurock <sblaurock@gmail.com>
2013-08-22 18:42:59 -0400

    Added missing comments.

--- 16/16.rb
+++ 16/16.rb
@@ -4,12 +4,14 @@ num = 2
 limit = 1000
 sum = 0
 
+# 'num' to the power of 'limit'
 i = 1
 while i < limit
 	num *= 2
 	i += 1
 end
 
+# Add all digits in 'num'
 num.to_s.split('').each { |j| sum += j.to_i }
 
 puts sum

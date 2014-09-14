da7c9cbf2241203a48c84bb317ed47f501b37056
Steve Blaurock <sblaurock@gmail.com>
2014-09-14 13:04:20 -0400

    Housekeeping. Spaces to tabs, consistency changes.

--- 23/23.rb
+++ 23/23.rb
@@ -6,48 +6,48 @@ limit = 28123
 
 # Calculate the sum of all divisors of a number
 def getDivisorSum num
-  _divisors = [1]
-  _limit = Math.sqrt(num)
-  _i = 2
+	_divisors = [1]
+	_limit = Math.sqrt(num)
+	_i = 2
 
-  while _i <= _limit do
-    if num % _i === 0
-      _divisors.push(num / _i) unless _divisors.include?(num / _i)
-      _divisors.push(_i) unless _divisors.include?(_i)
-    end
+	while _i <= _limit do
+		if num % _i === 0
+			_divisors.push(num / _i) unless _divisors.include?(num / _i)
+			_divisors.push(_i) unless _divisors.include?(_i)
+		end
 
-    _i += 1
-  end
+		_i += 1
+	end
 
-  return _divisors.inject(:+)
+	return _divisors.inject(:+)
 end
 
 i = 12
 while i <= limit do
-  abundantNumbers.push(i) if getDivisorSum(i) > i
+	abundantNumbers.push(i) if getDivisorSum(i) > i
 
-  i += 1;
+	i += 1;
 end
 
-# Get combined sum of every two abundant numbers and append to hash.
+# Get combined sum of every two abundant numbers and append to hash
 i = 0
 sum = 0
 length = abundantNumbers.count - 1
 while i < length do
-  j = i
+	j = i
 
-  while j < length do
-    sum = abundantNumbers[j] + abundantNumbers[i];
+	while j < length do
+		sum = abundantNumbers[j] + abundantNumbers[i];
 
-    break if sum > limit
+		break if sum > limit
 
-    abundantNumberSums[sum] = true
+		abundantNumberSums[sum] = true
 
-    j += 1
-  end
+		j += 1
+	end
 
-  i += 1
+	i += 1
 end
 
-# Sum every positive integer until 'limit' that isn't the sum of two abundant numbers.
+# Sum every positive integer until 'limit' that isn't the sum of two abundant numbers
 puts (0..limit).select{ |n| !abundantNumberSums.key?(n) }.inject(:+)
